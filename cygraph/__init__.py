aae594c1ad403102ed407c56e2f580616654f687
lol-cubes <arinmkhare@gmail.com>
2020-07-19 20:11:35 -0400

    write create_graph function

--- cygraph/__init__.py
+++ cygraph/__init__.py
@@ -1,4 +1,23 @@
 """A graph theory library for both python and cython.
 """
 
-from .graph import Graph
+from .graph import StaticGraph, DynamicGraph
+
+
+def create_graph(static=False, directed=False, vertices=[]):
+    """
+    Creates an instance of a Graph object.
+
+    Args:
+        static: Optional; Setting this to true makes overall operations
+            much faster, but it makes adding nodes much slower,
+            especially on large graphs.
+        directed: Optional; Whether or not the graph has directed edges.
+        vertices: Optional; A collection of initial vertices for the graph.
+    """
+
+    args = [directed, vertices]
+    if static:
+        return StaticGraph(*args)
+    else:
+        return DynamicGraph(*args)
