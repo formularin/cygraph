1de0ffcaafeb13ea7c3651b6cca812a1c58f5d57
lol-cubes <arinmkhare@gmail.com>
2020-08-22 11:48:47 -0400

    move sample graphs for unit tests into their respective functions

--- cygraph/tests/test_algorithms.py
+++ cygraph/tests/test_algorithms.py
@@ -18,54 +18,35 @@ from ..algorithms import (
 )
 
 
-# This graph has 5 vertices and has edges between all comibinations of vertices.
-sample_vertices = list(range(1, 6))
-sample_edges = list(itertools.combinations(sample_vertices, 2))
-SAMPLE_GRAPHS_1 = {
-    'static': {
-        'undirected': create_graph(static=True, directed=False, vertices=sample_vertices),
-        'directed': create_graph(static=True, directed=True, vertices=sample_vertices)
-    },
-    'dynamic': {
-        'undirected': create_graph(static=False, directed=False, vertices=sample_vertices),
-        'directed': create_graph(static=False, directed=True, vertices=sample_vertices)
-    }
-}
-for graph in [g for s in SAMPLE_GRAPHS_1 for g in SAMPLE_GRAPHS_1[s].values()]:
-    for edge in sample_edges:
-        if not graph.has_edge(*edge):
-            graph.add_edge(*edge)
-
-random_graph = {0: {74, 11, 76}, 1: {33, 59, 77}, 2: {53, 78}, 3: {64}, 4: {74}, 5: {80, 81, 89}, 6: {29, 70, 47}, 7: {8}, 8: {58, 36, 7, 95}, 9: {26, 23}, 10: set(), 11: {0, 70}, 12: {73}, 13: {99, 62, 39}, 14: set(), 15: {26, 50, 76}, 16: {93, 22}, 17: {84, 86}, 18: {50, 77}, 19: set(), 20: {56, 58, 66, 39}, 21: {97, 67, 71, 50, 28}, 22: {16, 77}, 23: {9}, 24: {49, 33}, 25: set(), 26: {9, 15}, 27: {41, 82, 33}, 28: {80, 21, 70}, 29: {43, 6}, 30: {73, 46}, 31: {80, 50, 82}, 32: {46}, 33: {1, 73, 80, 24, 57, 27}, 34: {48, 84, 70, 71}, 35: {65, 63}, 36: {8, 46, 47}, 37: set(), 38: {98, 85}, 39: {69, 20, 13, 54}, 40: {66}, 41: {56, 27}, 42: set(), 43: {88, 29}, 44: {68}, 45: {48}, 46: {32, 36, 30}, 47: {36, 6}, 48: {81, 34, 45}, 49: {24, 81, 54}, 50: {18, 15, 21, 31}, 51: set(), 52: set(), 53: {2}, 54: {65, 49, 39}, 55: {89}, 56: {65, 41, 73, 20, 59}, 57: {33}, 58: {8, 97, 20}, 59: {56, 1}, 60: set(), 61: set(), 62: {13}, 63: {35}, 64: {3}, 65: {56, 81, 35, 54}, 66: {40, 90, 20}, 67: {21}, 68: {44}, 69: {97, 93, 39}, 70: {34, 6, 11, 86, 28}, 71: {73, 34, 21}, 72: {98, 94}, 73: {33, 71, 12, 56, 30}, 74: {0, 4}, 75: set(), 76: {0, 15}, 77: {1, 18, 22}, 78: {2}, 79: set(), 80: {33, 5, 87, 28, 31}, 81: {65, 49, 48, 5}, 82: {27, 31}, 83: {87}, 84: {17, 34}, 85: {90, 38}, 86: {17, 70}, 87: {80, 83}, 88: {43}, 89: {5, 55}, 90: {66, 85}, 91: set(), 92: set(), 93: {16, 69}, 94: {72}, 95: {8}, 96: set(), 97: {58, 69, 21}, 98: {72, 38}, 99: {13}}
-SAMPLE_GRAPHS_2 = {
-    'static': {
-        'undirected': create_graph(static=True, directed=False, vertices=list(random_graph)),
-        'directed': create_graph(static=True, directed=True, vertices=list(random_graph))
-    },
-    'dynamic': {
-        'undirected': create_graph(static=False, directed=False, vertices=list(random_graph)),
-        'directed': create_graph(static=False, directed=True, vertices=list(random_graph))
-    }
-}
-for graph in [g for s in SAMPLE_GRAPHS_2 for g in SAMPLE_GRAPHS_2[s].values()]:
-    for vertex in random_graph:
-        for neighbor in random_graph[vertex]:
-            if not graph.has_edge(vertex, neighbor):
-                graph.add_edge(vertex, neighbor)
-
-
 def test_find_articulation_points():
     """
     Tests find_articulation_points function.
     """
+    random_graph = {0: {74, 11, 76}, 1: {33, 59, 77}, 2: {53, 78}, 3: {64}, 4: {74}, 5: {80, 81, 89}, 6: {29, 70, 47}, 7: {8}, 8: {58, 36, 7, 95}, 9: {26, 23}, 10: set(), 11: {0, 70}, 12: {73}, 13: {99, 62, 39}, 14: set(), 15: {26, 50, 76}, 16: {93, 22}, 17: {84, 86}, 18: {50, 77}, 19: set(), 20: {56, 58, 66, 39}, 21: {97, 67, 71, 50, 28}, 22: {16, 77}, 23: {9}, 24: {49, 33}, 25: set(), 26: {9, 15}, 27: {41, 82, 33}, 28: {80, 21, 70}, 29: {43, 6}, 30: {73, 46}, 31: {80, 50, 82}, 32: {46}, 33: {1, 73, 80, 24, 57, 27}, 34: {48, 84, 70, 71}, 35: {65, 63}, 36: {8, 46, 47}, 37: set(), 38: {98, 85}, 39: {69, 20, 13, 54}, 40: {66}, 41: {56, 27}, 42: set(), 43: {88, 29}, 44: {68}, 45: {48}, 46: {32, 36, 30}, 47: {36, 6}, 48: {81, 34, 45}, 49: {24, 81, 54}, 50: {18, 15, 21, 31}, 51: set(), 52: set(), 53: {2}, 54: {65, 49, 39}, 55: {89}, 56: {65, 41, 73, 20, 59}, 57: {33}, 58: {8, 97, 20}, 59: {56, 1}, 60: set(), 61: set(), 62: {13}, 63: {35}, 64: {3}, 65: {56, 81, 35, 54}, 66: {40, 90, 20}, 67: {21}, 68: {44}, 69: {97, 93, 39}, 70: {34, 6, 11, 86, 28}, 71: {73, 34, 21}, 72: {98, 94}, 73: {33, 71, 12, 56, 30}, 74: {0, 4}, 75: set(), 76: {0, 15}, 77: {1, 18, 22}, 78: {2}, 79: set(), 80: {33, 5, 87, 28, 31}, 81: {65, 49, 48, 5}, 82: {27, 31}, 83: {87}, 84: {17, 34}, 85: {90, 38}, 86: {17, 70}, 87: {80, 83}, 88: {43}, 89: {5, 55}, 90: {66, 85}, 91: set(), 92: set(), 93: {16, 69}, 94: {72}, 95: {8}, 96: set(), 97: {58, 69, 21}, 98: {72, 38}, 99: {13}}
+    sample_graphs = {
+        'static': {
+            'undirected': create_graph(static=True, directed=False, vertices=list(random_graph)),
+            'directed': create_graph(static=True, directed=True, vertices=list(random_graph))
+        },
+        'dynamic': {
+            'undirected': create_graph(static=False, directed=False, vertices=list(random_graph)),
+            'directed': create_graph(static=False, directed=True, vertices=list(random_graph))
+        }
+    }
+    for graph in [g for s in sample_graphs for g in sample_graphs[s].values()]:
+        for vertex in random_graph:
+            for neighbor in random_graph[vertex]:
+                if not graph.has_edge(vertex, neighbor):
+                    graph.add_edge(vertex, neighbor)
+
     random_graph_articulation_points = {33, 46, 48, 13, 39, 21, 89, 5, 87, 80, 35, 65, 73, 9, 26, 15, 72, 98, 38, 85, 90, 66, 20, 8, 43, 29, 6, 74, 0, 2}
 
-    for static in SAMPLE_GRAPHS_2:
-        g1 = SAMPLE_GRAPHS_2[static]['directed']
+    for static in sample_graphs:
+        g1 = sample_graphs[static]['directed']
         with pytest.raises(NotImplementedError):
             find_articulation_points(g1)
         
-        g2 = SAMPLE_GRAPHS_2[static]['undirected']
+        g2 = sample_graphs[static]['undirected']
         assert find_articulation_points(g2) == random_graph_articulation_points
 
 
@@ -127,7 +108,23 @@ def test_partition_graph():
     """
     Tests partition_graph function.
     """
-    for static in SAMPLE_GRAPHS_1:
+    sample_vertices = list(range(1, 6))
+    sample_edges = list(itertools.combinations(sample_vertices, 2))
+    sample_graphs = {
+        'static': {
+            'undirected': create_graph(static=True, directed=False, vertices=sample_vertices),
+            'directed': create_graph(static=True, directed=True, vertices=sample_vertices)
+        },
+        'dynamic': {
+            'undirected': create_graph(static=False, directed=False, vertices=sample_vertices),
+            'directed': create_graph(static=False, directed=True, vertices=sample_vertices)
+        }
+    }
+    for graph in [g for s in sample_graphs for g in sample_graphs[s].values()]:
+        for edge in sample_edges:
+            if not graph.has_edge(*edge):
+                graph.add_edge(*edge)
+    for static in sample_graphs:
         g2 = create_graph(static=(static == 'static'), directed=False, vertices=[])
         with pytest.raises(ValueError):
             partition_graph(g2)
@@ -135,11 +132,11 @@ def test_partition_graph():
         with pytest.raises(ValueError):
             partition_graph(g2)
 
-        g = SAMPLE_GRAPHS_1[static]['directed']
+        g = sample_graphs[static]['directed']
         with pytest.raises(NotImplementedError):
             partition_graph(g)
 
-    for graphs in SAMPLE_GRAPHS_1.values():
+    for graphs in sample_graphs.values():
         input_graph = graphs['undirected']
         output_g1, output_g2, cutset = partition_graph(input_graph)
         # Check that all edges and vertices of original graph are in
