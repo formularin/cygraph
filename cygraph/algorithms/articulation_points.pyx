f609fb5a94481ddce2eba71d707da8a19fa3f03e
lol-cubes <arinmkhare@gmail.com>
2020-08-26 14:58:41 -0400

    make variable names more descriptive in articulation points algorithm

--- cygraph/algorithms/articulation_points.pyx
+++ cygraph/algorithms/articulation_points.pyx
@@ -39,7 +39,7 @@ cdef set get_articulation_points(Graph graph):
     # Local loop variables.
     cdef object start, grandparent, parent, child
     cdef set children
-    cdef dict discovery, low
+    cdef dict discovery_times, lowpoints
     cdef int root_children
     cdef list stack
     for start in graph.vertices:
@@ -47,8 +47,8 @@ cdef set get_articulation_points(Graph graph):
         if start in visited:
             continue
         
-        discovery = {start: 0}  # time of first discovery of vertex during search
-        low = {start: 0}
+        discovery_times = {start: 0}  # time of first discovery of vertex during search
+        lowpoints = {start: 0}
         root_children = 0
         visited.add(start)
         stack = [(start, start, graph.get_children(start))]
@@ -59,18 +59,22 @@ cdef set get_articulation_points(Graph graph):
                 if grandparent == child:
                     continue
                 if child in visited:
-                    if discovery[child] <= discovery[parent]:  # back edge
-                        low[parent] = min(low[parent], discovery[child])
+                    if discovery_times[child] <= discovery_times[parent]:
+                        # back edge
+                        lowpoints[parent] = \
+                            min(lowpoints[parent], discovery_times[child])
                 else:
-                    low[child] = discovery[child] = len(discovery)
+                    lowpoints[child] = discovery_times[child] = \
+                        len(discovery_times)
                     visited.add(child)
                     stack.append((parent, child, graph.get_children(child)))
             except KeyError:  # Iterated through all children.
                 stack.pop()
                 if len(stack) > 1:
-                    if low[parent] >= discovery[grandparent]:
+                    if lowpoints[parent] >= discovery_times[grandparent]:
                         articulation_points.add(grandparent)
-                    low[grandparent] = min(low[parent], low[grandparent])
+                    lowpoints[grandparent] = min(lowpoints[parent],
+                                                 lowpoints[grandparent])
                 elif stack:  # length 1 so grandparent is root
                     root_children += 1
         # root is articulation point if it has more than 1 child
