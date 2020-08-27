d1ccc33e1570055426287654f65b3e1626df5d0e
lol-cubes <arinmkhare@gmail.com>
2020-08-27 15:01:44 -0400

    remove examlpes dir

--- examples/cython/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-import numpy as np
-from Cython.Build import cythonize
-
-setup(
-    ext_modules=cythonize(),
-    include_dirs=[np.get_include()]
-)
--- examples/cython/traveling_saleman.pyx
+++
@@ -1,5 +0,0 @@
-#!python
-#cython: language_level=3
-"""Implementation of the ant colony optimization (ACO) algorithm for the
-traveling salesman problem (TSP).
-"""
