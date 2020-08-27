b68e460bf6183895d07c51ca544af3e511682a79
lol-cubes <arinmkhare@gmail.com>
2020-08-27 17:53:00 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
 import sys
 from setuptools import setup, find_packages
+from setuptools import dist
+
+dist.Distribution().fetch_build_eggs(['Cython>=0.29.21', 'numpy>=1.19.0'])
+
+import numpy as np
+from Cython.Build import cythonize
 
 
-if sys.argv[1:3] == ['build_ext', '--inplace']:
-    # Compile and install cython code.
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
-    import numpy as np
-    from Cython.Build import cythonize
 
-    setup(
-        ext_modules=cythonize(
-            ['cygraph/*.pyx', 'cygraph/algorithms/*.pyx',
-             'cygraph/graph_/*.pyx']
-        ),
-        # ext_modules=cythonize('test.pyx'),
-        include_dirs=[np.get_include()]
-    )
-
-else:
-    # Install the package without compiling cython code.
-    setup(
-        name='cygraph',
-        packages=find_packages(),
-        install_requires=[
-            'cython',
-            'numpy'
-        ],
-    )
+setup(
+    name='cygraph',
+    version='0.1.0',
+    author='Arin Khare',
+    author_email='arinmkhare@gmail.com',
+    description='A graph theory library written in Cython',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/lol-cubes/cygraph',
+    packages=find_packages(),
+    ext_modules=cythonize(
+        ['cygraph/*.pyx', 'cygraph/algorithms/*.pyx',
+            'cygraph/graph_/*.pyx']
+    ),
+    include_dirs=[np.get_include()]
+)
