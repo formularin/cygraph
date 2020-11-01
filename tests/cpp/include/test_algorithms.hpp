4667416b5159e832a6afabec2cec58f5ea7a3817
lol-cubes <arinmkhare@gmail.com>
2020-10-31 20:40:21 -0400

    add empty test fixtures for all algorithm functions

--- tests/cpp/include/test_algorithms.hpp
+++ tests/cpp/include/test_algorithms.hpp
@@ -24,4 +24,34 @@ class TestArticulationPoints: public CppUnit::TestFixture {
         static CppUnit::Test* suite();
 };
 
+
+class TestComponents: public CppUnit::TestFixture {
+    public:
+        void setUp();
+        void tearDown();
+        void test_get_connected_components();
+        void test_get_number_connected_components();
+        void test_get_strongly_connected_components();
+        void test_get_number_strongly_connected_components();
+        static CppUnit::Test* suite();
+};
+
+
+class TestPartitioning: public CppUnit::TestFixture {
+    public:
+        void setUp();
+        void tearDown();
+        void test_partition_karger();
+        static CppUnit::Test* suite();
+};
+
+
+class TestShortestPath: public CppUnit::TestFixture {
+    public:
+        void setUp();
+        void tearDown();
+        void test_get_shortest_path_dijkstra();
+        static CppUnit::Test* suite();
+};
+
 #endif
--- tests/cpp/src/test_algorithms.cpp
+++ tests/cpp/src/test_algorithms.cpp
@@ -6,25 +6,113 @@ Implementation of algorithm test fixtures.
 #include "../include/test_graph.hpp"
 
 
+// ARTICULATION POINTS
+// ===================
+
+
 void TestArticulationPoints::setUp() {}
-
 void TestArticulationPoints::tearDown() {}
+void TestArticulationPoints::test_get_articulation_points() {}
 
-void TestArticulationPoints::test_get_articulation_points() {
-    CPPUNIT_ASSERT( 1 == 1 );
-}
 
 CppUnit::Test* TestArticulationPoints::suite() {
-    /*
-    Creates a suite of tests containing all of the test methods in this
-    class.
-    */
 
     CppUnit::TestSuite *test_suite =
         new CppUnit::TestSuite("TestArticulationPoints");
     test_suite->addTest(
-            new CppUnit::TestCaller<TestArticulationPoints>(
-                "test_get_articulation_points",
-                &TestArticulationPoints::test_get_articulation_points));
+        new CppUnit::TestCaller<TestArticulationPoints>(
+            "test_get_articulation_points",
+            &TestArticulationPoints::test_get_articulation_points
+        )
+    );
+    return test_suite;
+}
+
+
+// COMPONENTS
+// ==========
+
+
+void TestComponents::setUp() {}
+void TestComponents::tearDown() {}
+void TestComponents::test_get_connected_components() {}
+void TestComponents::test_get_number_connected_components() {}
+void TestComponents::test_get_strongly_connected_components() {}
+void TestComponents::test_get_number_strongly_connected_components() {}
+
+
+CppUnit::Test* TestComponents::suite() {
+
+    CppUnit::TestSuite *test_suite =
+        new CppUnit::TestSuite("TestComponents");
+    test_suite->addTest(
+        new CppUnit::TestCaller<TestComponents>(
+            "test_get_connected_components",
+            &TestComponents::test_get_connected_components
+        )
+    );
+    test_suite->addTest(
+        new CppUnit::TestCaller<TestComponents>(
+            "test_get_number_connected_components",
+            &TestComponents::test_get_number_connected_components
+        )
+    );
+    test_suite->addTest(
+        new CppUnit::TestCaller<TestComponents>(
+            "test_get_strongly_connected_components",
+            &TestComponents::test_get_strongly_connected_components
+        )
+    );
+    test_suite->addTest(
+        new CppUnit::TestCaller<TestComponents>(
+            "test_get_number_strongly_connected_components",
+            &TestComponents::test_get_number_strongly_connected_components
+        )
+    );
+    return test_suite;
+}
+
+
+// PARTITIONING
+// ============
+
+
+void TestPartitioning::setUp() {}
+void TestPartitioning::tearDown() {}
+void TestPartitioning::test_partition_karger() {}
+
+
+CppUnit::Test* TestPartitioning::suite() {
+
+    CppUnit::TestSuite *test_suite =
+        new CppUnit::TestSuite("TestPartitioning");
+    test_suite->addTest(
+        new CppUnit::TestCaller<TestPartitioning>(
+            "test_partition_karger",
+            &TestPartitioning::test_partition_karger
+        )
+    );
+    return test_suite;
+}
+
+// SHORTEST PATH
+// =============
+
+
+void TestShortestPath::setUp() {}
+void TestShortestPath::tearDown() {}
+void TestShortestPath::test_get_shortest_path_dijkstra() {}
+
+
+CppUnit::Test* TestShortestPath::suite() {
+
+    CppUnit::TestSuite *test_suite =
+        new CppUnit::TestSuite("TestShortestPath");
+    test_suite->addTest(
+        new CppUnit::TestCaller<TestShortestPath>(
+            "test_get_shortest_path_dijkstra",
+            &TestShortestPath::test_get_shortest_path_dijkstra
+        )
+    );
     return test_suite;
 }
